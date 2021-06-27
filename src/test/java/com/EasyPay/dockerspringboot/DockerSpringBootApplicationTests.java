99506f7502532d69a23fb59235bdccaede46f78c
ManuelCanelon <manucanelonrojas@gmail.com>
2021-06-27 13:15:55 -0600

    first commit

---
+++ src/test/java/com/EasyPay/dockerspringboot/DockerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EasyPay.dockerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
